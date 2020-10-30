df3fe259f64f0dbfadc16f2a93fb39476f041311
neil <neil.liu@easemob.com>
2020-10-30 11:05:34 +0800

    override onItemClick method in ConversationListFragment

--- app/src/main/java/com/example/easeimdemo/ui/ConversationListFragment.java
+++ app/src/main/java/com/example/easeimdemo/ui/ConversationListFragment.java
@@ -1,8 +1,16 @@
 package com.example.easeimdemo.ui;
 
+import android.view.View;
+import android.widget.Toast;
+
 import com.hyphenate.easeui.ui.EaseConversationListFragment;
 
 public class ConversationListFragment extends EaseConversationListFragment {
 
+    @Override
+    public void onItemClick(View view, int position) {
+        super.onItemClick(view, position);
+        Toast.makeText(mContext, "点击了条目："+position, Toast.LENGTH_SHORT).show();
+    }
 }
 
